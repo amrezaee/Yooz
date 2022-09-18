e2f83a273f40e1f55f4f2e9dacea3cff2b1d16fc
AM.Rezaee <45487536+amrezaee@users.noreply.github.com>
2022-09-18 15:17:56 +0430

    add make_unique for C++11

--- Yooz/yzStds.hpp
+++ Yooz/yzStds.hpp
@@ -2,7 +2,6 @@
 // Precompiled header for standard library
 
 #include <algorithm>
-#include <cassert>  // TODO: remove this
 #include <chrono>
 #include <cmath>
 #include <cstddef>
@@ -15,3 +14,43 @@
 #include <type_traits>
 #include <utility>
 #include <vector>
+
+// make_unique for c++11
+#if __cplusplus == 201103L
+
+namespace std
+{
+namespace detail
+{
+	template<class>
+	constexpr bool is_unbounded_array_v = false;
+
+	template<class T>
+	constexpr bool is_unbounded_array_v<T[]> = true;
+
+	template<class>
+	constexpr bool is_bounded_array_v = false;
+
+	template<class T, std::size_t N>
+	constexpr bool is_bounded_array_v<T[N]> = true;
+}  // namespace detail
+
+template<class T, class... Args>
+std::enable_if_t<!std::is_array<T>::value, std::unique_ptr<T>> make_unique(
+        Args&&... args)
+{
+	return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+
+template<class T>
+std::enable_if_t<detail::is_unbounded_array_v<T>, std::unique_ptr<T>>
+make_unique(std::size_t n)
+{
+	return std::unique_ptr<T>(new std::remove_extent_t<T>[n]());
+}
+
+template<class T, class... Args>
+std::enable_if_t<detail::is_bounded_array_v<T>> make_unique(Args&&...) = delete;
+
+}  // namespace std
+#endif
