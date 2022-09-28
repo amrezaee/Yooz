7a159888691d465bb2b1e45c464732cf740cbbe3
amrezaee <amrezaee2@gmail.com>
2022-09-28 15:04:19 +0330

    refactor and add some easing functions

--- Yooz/Math/yzFunctions.cpp
+++ Yooz/Math/yzFunctions.cpp
@@ -2,15 +2,38 @@
 
 namespace yz
 {
-float SmoothStep(float a, float b, float t)
+float SmoothStep(const float a, const float b, const float t)
 {
-	t = Clamp((t - a) / (b - a), 0.0f, 1.0f);
-	return t * t * (3.0f - 2.0f * t);
+	float v = Clamp((t - a) / (b - a), 0.0f, 1.0f);
+	return v * v * (3.0f - 2.0f * v);
 }
 
-float SmootherStep(float a, float b, float t)
+float SmootherStep(const float a, const float b, const float t)
 {
-	t = Clamp((t - a) / (b - a), 0.0f, 1.0f);
-	return t * t * t * (t * (t * 6.0f - 15.0f) + 10.0f);
+	float v = Clamp((t - a) / (b - a), 0.0f, 1.0f);
+	return v * v * v * (v * (v * 6.0f - 15.0f) + 10.0f);
+}
+
+// Easing functions
+float EaseInSin(const float x) { return 1.0f - std::cos(x * CPI * 0.5f); }
+float EaseOutSin(const float x) { return std::sin(x * CPI * 0.5f); }
+float EaseInOutSin(const float x) { return -(std::cos(x * CPI) - 1.0f) * 0.5f; }
+float EaseOutQuart(const float x)
+{
+	float t = (x - 1.0f) * x;
+	return 1.0f - t * t;
+}
+float EaseInOutQuart(const float x)
+{
+	if(x < 0.5f)
+	{
+		float t = x * x;
+		return 8.0f * t * t;
+	}
+	else
+	{
+		float t = (x - 1.0f) * x;
+		return 1.0f - 8.0f * t * t;
+	}
 }
 }  // namespace yz
--- Yooz/Math/yzFunctions.hpp
+++ Yooz/Math/yzFunctions.hpp
@@ -6,25 +6,25 @@
 
 namespace yz
 {
-constexpr float Deg2Rad(float deg) { return deg * CPI_180; }
-constexpr float Rad2Deg(float rad) { return rad * C180_PI; }
+constexpr float Deg2Rad(const float deg) { return deg * CPI_180; }
+constexpr float Rad2Deg(const float rad) { return rad * C180_PI; }
 
 template<typename T>
 constexpr const T& Clamp(const T& v, const T& l, const T& h)
 {
-	return v < l ? l : (h < v ? h : v);
+	return (v < l) ? l : ((h < v) ? h : v);
 }
 
 template<typename T>
 constexpr const T& Max(const T& a, const T& b)
 {
-	return a < b ? b : a;
+	return (a < b) ? b : a;
 }
 
 template<typename T>
 constexpr const T& Min(const T& a, const T& b)
 {
-	return a < b ? a : b;
+	return (a < b) ? a : b;
 }
 
 // clang-format off
@@ -38,7 +38,7 @@ Sign(const T v)
 	return (static_cast<T>(0) < v) - (v < static_cast<T>(0));
 }
 
-constexpr float Lerp(float a, float b, float t) { return (1.0f - t) * a + t * b; }
+constexpr float Lerp(const float a, const float b, const float t) { return (1.0f - t) * a + t * b; }
 
 template<typename T>
 constexpr 
@@ -49,8 +49,34 @@ Lerp(const T a, const T b, const T t)
 }
 // clang-format on
 
-float SmoothStep(float a, float b, float t);
+float SmoothStep(const float a, const float b, const float t);
 
-float SmootherStep(float a, float b, float t);
+float SmootherStep(const float a, const float b, const float t);
 
+// Easing functions
+float EaseInSin(const float x);
+float EaseOutSin(const float x);
+float EaseInOutSin(const float x);
+
+constexpr float EaseInQuad(const float x) { return x * x; }
+constexpr float EaseOutQuad(const float x) { return x * (2.0f - x); }
+constexpr float EaseInOutQuad(const float x)
+{
+	return (x < 0.5f) ? (2.0f * x * x) : (x * (4.0f - 2.0f * x) - 1.0f);
+}
+
+constexpr float EaseInCubic(const float x) { return x * x * x; }
+constexpr float EaseOutCubic(const float x)
+{
+	return x * (x * x - 3.0f * x + 3.0f);
+}
+constexpr float EaseInOutCubic(const float x)
+{
+	return (x < 0.5f) ? (4.0f * x * x * x) :
+	                    (x * (4.0f * x * x - 12.0f * x + 12.0f) - 3.0f);
+}
+
+constexpr float EaseInQuart(const float x) { return x * x * x * x; }
+float           EaseOutQuart(const float x);
+float           EaseInOutQuart(const float x);
 }  // namespace yz
