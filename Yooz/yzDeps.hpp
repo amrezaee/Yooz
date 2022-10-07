ebf2c532e72dc67adcc1b6d402e4f14592370f0a
amrezaee <amrezaee2@gmail.com>
2022-10-07 17:00:20 +0330

    remove vcpkg glad and use external header file and add ghc-filesystem

--- Yooz/yzDeps.hpp
+++ Yooz/yzDeps.hpp
@@ -1,5 +1,13 @@
-#pragma once
 // Precompiled header for external dependencies
+#pragma once
+
+#ifdef YZ_GL_PLATFORM
+	#define GLAD_GL_IMPLEMENTATION
+	#include <glad/gl.h>
+#endif
+
+#include <ghc/filesystem.hpp>
+namespace fs = ghc::filesystem;
 
 #include <SDL.h>
-#include <glad/glad.h>
+
